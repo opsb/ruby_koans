607c568c91931bc0bf8dad4000dd126e216d984c
opsb <oliver@opsb.co.uk>
2010-09-21 19:31:33 +0100

    Finished triangle project

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  { 1 => :equilateral, 2 => :isosceles, 3 => :scalene }[
+    [a, b, c].uniq.length
+  ]
 end
 
 # Error class used in part 2.  No need to change this code.
